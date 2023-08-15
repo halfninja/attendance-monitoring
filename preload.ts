f5f43fff77293e30e5fa4461f9737ccb128840d5
Saturn Maoudis <saturnmaoudis@protonmail.com>
2023-08-15 15:50:05 +0100

    TESTS STARTING HERE

--- preload.ts
+++ preload.ts
@@ -1,7 +1,6 @@
 import { contextBridge } from 'electron';
 import { setupConnection, formattedData, cardReaders } from "./modules";
 
-
 // give the main window access to the contextBridge
 contextBridge.exposeInMainWorld('electron', {
     formattedData: () => {return formattedData;},
